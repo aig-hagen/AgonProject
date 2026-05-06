d6c8fdaf7afe976e6bc773e6bf133cc9c180cf6b
Oleksandr Dzhychko <oleksandr.dzhychko@fernuni-hagen.de>
2026-05-06 20:31:42 +0000

    chore: add script to upload docker images

---
+++ deployment/upload-docker-images.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+REMOTE="${1:?❌ Error: REMOTE is missing (arg 1, e.g. user@example.com)}"
+TARGET_TAG="${2:?❌ Error: TARGET_TAG is missing (arg 2, e.g. v0.0.1)}"
+
+function upload_image {
+  local source_image=$1
+  local target_image=$2
+
+  docker tag "${source_image}" "${target_image}"
+  echo "📤 Uploading ${source_image} as ${target_image} to ${REMOTE}."
+  docker image save "${target_image}" \
+  | gzip -9 \
+  | ssh "${REMOTE}" "gunzip | docker image load"
+
+  echo "✅ Uploaded ${source_image} as ${target_image} to ${REMOTE}."
+}
+
+upload_image "aig-hagen/argumentation-toolbox-frontend:latest" "aig-hagen/argumentation-toolbox-frontend:${TARGET_TAG}"
+upload_image "aig-hagen/argumentation-toolbox-backend:latest" "aig-hagen/argumentation-toolbox-backend:${TARGET_TAG}"
