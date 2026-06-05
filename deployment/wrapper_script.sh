5cff9944be2997a467d58cb06ae6352590d77e02
Lars Bengel <lars.bengel@fernuni-hagen.de>
2026-06-05 09:31:58 +0200

    feat: enhance Docker setup with graph generation server and update Caddyfile for routing

--- deployment/wrapper_script.sh
+++ deployment/wrapper_script.sh
@@ -6,6 +6,9 @@
 java -jar ./web.jar --logging.config=./logback.xml --server.port=8081 &
 
 # Start the second process
+/opt/graph-gen-venv/bin/uvicorn server:app --app-dir /opt/graph-gen-server --host 127.0.0.1 --port 8082 &
+
+# Start the third process
 ./caddy run --config ./Caddyfile &
 
 # Wait for any process to exit
