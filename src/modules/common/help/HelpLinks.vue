d4341bef79e0ca25cd68af068ac76f7ac9dfbaa7
Lars Bengel <lars.bengel@fernuni-hagen.de>
2026-06-11 16:44:50 +0200

    feat: add GithubMarkIcon component and integrate Tweety logo in HelpLinks

--- src/modules/common/help/HelpLinks.vue
+++ src/modules/common/help/HelpLinks.vue
@@ -17,12 +17,9 @@
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -->
 <script setup lang="ts">
-import {
-  AcademicCapIcon,
-  ArrowUpRightIcon,
-  CodeBracketIcon,
-  UserGroupIcon,
-} from '@heroicons/vue/24/outline'
+import { ArrowUpRightIcon, UserGroupIcon } from '@heroicons/vue/24/outline'
+
+import GithubMarkIcon from '@/modules/common/help/GithubMarkIcon.vue'
 
 const sourceTree = import.meta.env.VITE_APP_SOURCE_TREE
 const sourceLink = `https://github.com/aig-hagen/argumentation-toolbox/tree/${sourceTree}`
@@ -34,10 +31,17 @@ const sourceLink = `https://github.com/aig-hagen/argumentation-toolbox/tree/${so
       target="_blank"
       rel="noopener"
       href="https://www.fernuni-hagen.de/aig/en/"
-      ><AcademicCapIcon class="size-4" />AIG Hagen<ArrowUpRightIcon class="size-2"
+      ><img src="/favicon-32x32.png" class="size-4" alt="" />AIG Hagen<ArrowUpRightIcon class="size-2"
+    /></a>
+    <a
+      class="btn btn-xs btn-soft"
+      target="_blank"
+      rel="noopener"
+      href="https://tweetyproject.org"
+      ><img src="/tweety-logo.png" class="size-4" alt="" />TweetyProject<ArrowUpRightIcon class="size-2"
     /></a>
     <a class="btn btn-xs btn-soft" target="_blank" rel="noopener" :href="sourceLink"
-      ><CodeBracketIcon class="size-4" />Source {{ sourceTree }}<ArrowUpRightIcon class="size-2"
+      ><GithubMarkIcon class="size-4" />Source {{ sourceTree }}<ArrowUpRightIcon class="size-2"
     /></a>
     <a class="btn btn-xs btn-soft" target="_blank" rel="noopener" href="/third-party"
       ><UserGroupIcon class="size-4" />Third-Party<ArrowUpRightIcon class="size-2"
