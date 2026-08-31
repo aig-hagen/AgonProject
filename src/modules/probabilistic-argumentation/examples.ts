a40ad36d15ca010f4555230669a8deac8a975dff
Lars Bengel <lars.bengel@fernuni-hagen.de>
2026-08-31 20:44:34 +0200

    feat: add software release example to probabilistic argumentation module

--- src/modules/probabilistic-argumentation/examples.ts
+++ src/modules/probabilistic-argumentation/examples.ts
@@ -20,13 +20,14 @@ import type { Example } from '@/modules/common/examples'
 import { getNodePositions } from '@/modules/common/graph-editor/layouting'
 import { Layout } from '@/modules/common/main-menu/layouting'
 import murderTrialJson from '@/modules/probabilistic-argumentation/examples/murder_trial.json'
+import softwareReleaseJson from '@/modules/probabilistic-argumentation/examples/software_release.json'
 import {
   type PafArgumentData,
   ProbabilisticArgumentation,
 } from '@/modules/probabilistic-argumentation/model'
 import { loadExampleFromJson } from '@/modules/probabilistic-argumentation/save/saveFormat'
 
-const exampleJsons: unknown[] = [murderTrialJson]
+const exampleJsons: unknown[] = [murderTrialJson, softwareReleaseJson]
 
 export const datasets: Example<ProbabilisticArgumentation<PafArgumentData>>[] = exampleJsons.map(
   (json) => {
