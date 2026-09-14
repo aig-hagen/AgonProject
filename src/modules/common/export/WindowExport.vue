0012a6f269d5ddd6b2b25b2fdb3c907b8303dcf8
Lars Bengel <lars.bengel@fernuni-hagen.de>
2026-09-14 22:26:11 +0200

    fix: bump LaTeX studio window height to avoid slight scroll

--- src/modules/common/export/WindowExport.vue
+++ src/modules/common/export/WindowExport.vue
@@ -295,7 +295,7 @@ onBeforeUnmount(() => {
     v-model:open="open"
     :title="t('menu.latexStudio')"
     :initial-position="{ x: 64, y: 128 }"
-    :intitalSize="{ width: 720, height: 500 }"
+    :intitalSize="{ width: 720, height: 540 }"
   >
     <ExportSheet
       v-if="layoutMode === 'compact'"
